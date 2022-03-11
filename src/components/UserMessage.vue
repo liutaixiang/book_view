393962aa3c9ad9d12f60c7b9337bc47e38086b23
liutaixiang <18311344865@163.com>
2022-03-11 15:38:12 +0800

    提交一下组件

---
+++ src/components/UserMessage.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="header">
+    <<router-link to="/loginPage">
+      <div class="header_menu">登录</div>
+    </router-link>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style scoped>
+  .header {
+    width: 100%;
+    height: 30px;
+    position: fixed;
+    left: 0;
+    top: 0;
+    color: #000;
+    background-color: #C3BD5C;
+  }
+  .header_menu {
+    padding=left: 60px;
+    padding-top: 10px;
+    float: right;
+    color: #fff;
+    font-size: 8px;
+  }
+</style>
